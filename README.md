eceffeda27d43996d13d1a84a91fdca30becdebd
muham08 <muham08@gmail.com>
2022-09-01 21:36:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Introduction
+2. Create React App
+3. Push to repository
+
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">kuchigi</a>
+      </header>
+      <main>List products</main>
+      <footer>Copyright (c)</footer>
+    </div>
+  );
+}
+
+export default App;
